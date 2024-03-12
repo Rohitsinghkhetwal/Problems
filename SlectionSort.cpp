dc5c9a49471a217527b9cb4b153235865607fd83
rohit <raj0eshsing0909@gmail.com>
2024-03-13 00:48:02 +0530

    bubble sort algorithm added

--- SlectionSort.cpp
+++ SlectionSort.cpp
@@ -14,7 +14,19 @@ void selection_sort(int arr[], int n){
         arr[minimum] = arr[i];
         arr[i] = temp;
     }
-    
+}
+
+void Bubble_sort(int arr[], int n){
+    for(int i = n -1; i >= 0; i--){
+        for(int j = 0; j <= i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
 }
 
 int main(){
@@ -25,7 +37,8 @@ int main(){
         cin >> arr[i];
     }
 
-    selection_sort(arr, n);
+    // selection_sort(arr, n);
+    Bubble_sort(arr, n);
     for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
